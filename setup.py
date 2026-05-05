9c63cbf404fd7b463502ba6f04968285b044b88d
Farouk Bello <faroukk.bello@gmail.com>
2026-05-05 14:11:54 +0100

    feat: added gitignore and setup

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name="md2pdf",
     version="0.1.0",
-    packages=find_packages(),
+    py_modules=["md2pdf"],
     install_requires=["markdown", "weasyprint"],
+    entry_points={
+        "console_scripts": [
+            "md2pdf=md2pdf:main",
+        ],
+    },
     author="Maverick",
     description="A tool to convert markdown to PDF",
     python_requires=">=3.6",
